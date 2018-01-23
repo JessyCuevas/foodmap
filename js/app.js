c49a545f61f7d3f53edb6b0addc68f2bced85849
JessyCuevas <jessy.cuevas.aranda@gmail.com>
2018-01-23 13:59:53 -0600

    Web-App

--- js/app.js
+++ js/app.js
@@ -1,64 +1,66 @@
 
-//-----Data-----//
-//var data = [
-//  {'name': 'The Capital Grille',
-//   'adress':'Paseo de la Reforma 250, Juárez. Dentro del Edificio Capital Reforma, esq. con Niza.',
-//   'photo':,
-//   'food': 'China'
-//    }
-//  {'name': 'Buena Barra',
-//   'adress':'Paseo2 de la Reforma 250, Juárez. Dentro del Edificio Capital Reforma, esq. con Niza.',
-//   'photo':,
-//   'food': 'Italiana'}
-//  {
-//    'name': 'The Palm Santa Fe',
-//     'adress':'Paseo3 de la Reforma 250, Juárez. Dentro del Edificio Capital Reforma, esq. con Niza.',
-//     'photo':,
-//     'food': 'Argentina'
-//  }
-//  {
-//    'name': 'Bellini',
-//     'adress':'Paseo4 de la Reforma 250, Juárez. Dentro del Edificio Capital Reforma, esq. con Niza.',
-//     'photo':,
-//     'food': 'Italiana'
-//  }
-//  {
-//    'name': 'Restaurante Loma Linda',
-//     'adress':'Paseo5 de la Reforma 250, Juárez. Dentro del Edificio Capital Reforma, esq. con Niza.',
-//     'photo':,
-//     'food': 'Mexicana'
-//  }
-//  {
-//    'name': 'Miralto',
-//     'adress':'Paseo6 de la Reforma 250, Juárez. Dentro del Edificio Capital Reforma, esq. con Niza.',
-//     'photo':,
-//     'food': 'Portuguesa'
-//  }
-//  {
-//    'name': 'Mexicanito',
-//     'adress':'Paseo7 de la Reforma 250, Juárez. Dentro del Edificio Capital Reforma, esq. con Niza.',
-//     'photo':,
-//     'food': 'Mexicana'
-//  }
-//]
 
 
-//-----Funciones-----//
-
+var data = data;
+//*Modal*//
+$(document).ready(function() {
+   $('.modal').modal();  
 
 //*Splash*//
-$(document).ready(function() {
     setTimeout(function() {
         $(".content").fadeIn(1500).fadeOut(1500);
     },1000);
-  
-  setTimeout(function() {
+  	setTimeout(function() {
         $(".content2").fadeIn(1500);
     },3000);
+
+//*Pintar-Info*//
+	//pintarInfo()
+    //$('#input').keyup(filterRestaurants)
+    //$('.modal').modal();
+    //$('a').click(paintModal)
+
+paintModal()
+$(".modal1").keyup();
+
+//-----Funciones para pintar info-----//
+function paintModal() {
+    var name = $(this).find('h4').text()
+    console.log(this);
+
+    var adress = $(this).find('.adress').text()
+    var food = $(this).find('.food').text()
+    var time = $(this).find('.time').text()
+    var pic = $(this).find('.pic').attr('src')
+    console.log(pic)
+    $('#name-modal').text(name)
+    $('#adress-modal').text(adress)
+    $('#food-modal').text(food)
+    $('#time-modal').text(time)
+    $('#pic-modal').attr('src', pic)
+}
+
+//function pintarInfo() {
+//    for (var i = 0; i < data.length; i++) {
+//        var $gralInfo = $('<div />', { 'class': 'square' });	
+//        var $infoCard = $('<a />', { 'class': 'waves-effect waves-light modal-trigger' });
+//        $infoCard.text('<p />', 'test!');
+//        $infoCard.attr('href', '#modal1');
+//        var $infoName = $('<h4 />');
+//        var $infoAdress = $('<p />', { 'class': 'adress' });
+//        var $infoImages = $('<img />', { 'class': 'images' });
+//        $infoImages.attr('src', data[i]['photo']);
+
+
+        //var $infoFood = $('<p />', { 'class': 'food' });
+        //var $infoTime = $('<p />', { 'class': 'time' });
+        //$infoName.append(data[i]['name']);
+        //$infoAdress.append(data[i]['adress']);
+        //$infoFood.append('Comida: ' + data[i]['food']);
+        //$rinfoTime.append('Horario: ' + data[i]['horarios']);
+        //$('.content2').append($gralInfo);
+//    }
+//}
+
+
 });
-
-//-----Modal-----//
-
-$(document).ready(function(){
-    $('.modal').modal();        //activates all the modals
-  });
---
+++ js/data.js
@@ -0,0 +1,15 @@
+
+//-----Data-----//
+var data = [{
+        'name': 'Gatolicos',
+        'adress': 'Tabasco 337, Cuauhtémoc, 06700 Ciudad de México, CDMX',
+        'food': 'Carne',
+        'photo': 'http://www.lagateria.com/wp-content/uploads/2015/05/Cafe3-1024x768.jpg',
+    },
+
+      'name': 'VeganCat',
+        'adress': 'Tabasco 337, Cuauhtémoc, 06700 Ciudad de México, CDMX',
+        'food': 'Vegan',
+        'photo': 'http://www.lagateria.com/wp-content/uploads/2015/05/Cafe3-1024x768.jpg',
+    },
+]
