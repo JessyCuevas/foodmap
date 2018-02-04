6835b951a53c46d3bc1eb1725caeab4de493be75
JessyCuevas <jessy.cuevas.aranda@gmail.com>
2018-02-03 19:13:34 -0600

    actualizacionModal-Data

--- js/app.js
+++ js/app.js
@@ -1,5 +1,3 @@
-var data = data;
-
 
 //*Splash*//
     setTimeout(function() {
@@ -11,53 +9,81 @@ var data = data;
 
 //*Modal*//
 $(document).ready(function() {
-   $('.modal').modal();  
 
+    $(".modal").modal();
 
+    paintPlaces()
+    $("#input").keyup(filterPlaces);
 
-//Pintar-Info*//
-	pintarInfo()
-    $('#input').keyup(filterRestaurants)
-    $('.modal').modal();
-    $('a').click(paintModal)
-
-//-----Funciones para pintar info-----//
-function paintModal() {
-    var name = $(this).find('h4').text()
-    console.log(this);
-
-    var adress = $(this).find('.adress').text()
-    var food = $(this).find('.food').text()
-    var time = $(this).find('.time').text()
-    var pic = $(this).find('.pic').attr('src')
-    console.log(pic)
-    $('#name-modal').text(name)
-    $('#adress-modal').text(adress)
-    $('#food-modal').text(food)
-    $('#time-modal').text(time)
-    $('#pic-modal').attr('src', pic)
-}
-
-function pintarInfo() { 
-    console.log(data);
-    for (var i = 0; i < data.length; i++) {
-        var $gralInfo = $('<div />', { 'class': 'square' });	
-        var $infoCard = $('<a />', { 'class': 'waves-effect waves-light modal-trigger' });
-        $infoCard.text('<p />', 'test!');
-        $infoCard.attr('href', '#modal1');
-        var $infoName = $('<h4 />');
-        var $infoAdress = $('<p />', { 'class': 'adress' });
-        var $infoImages = $('<img />', { 'class': 'images' });
-        //$infoImages.attr('src', data[i]['photo']);
-        //var $infoFood = $('<p />', { 'class': 'food' });
-        //var $infoTime = $('<p />', { 'class': 'time' });
-        //$infoName.append(data[i]['name']);
-        //$infoAdress.append(data[i]['adress']);
-        //$infoFood.append('Comida: ' + data[i]['food']);
-        //$rinfoTime.append('Horario: ' + data[i]['horarios']);
-        //$('.content2').append($gralInfo);
-    }
-}
+     $("a").click(modalPlaces)
 
 
 });
+
+function paintPlaces() {
+    for (var i = 0; i < data.length; i++) {
+
+    var $row = $("<div />").addClass("row");
+        var $col = $("<div />").addClass("col s12 m6");
+            var $card = $("<div />").addClass("card");
+                var $cardImage = $("<div />").addClass("card-image"); 
+                    var $imgCard = $("<img />").attr("src", data[i]["photo"]);
+                    var $btn = $ ("<a />").addClass("btn-floating halfway-fab waves-effect waves-light red modal-trigger");
+                    $btn.attr("href", "#modal1");
+                    var $cardIcon = $("<i />").addClass("material-icons");
+    
+                var $cardContent = $("<div />").addClass("card-content");
+                    var $cardTitle = $("<span />").text(data[i]["name"]).addClass("card-title");
+                    var $cardMap = $("<p />").addClass("");
+                    var $cardDescription = $("<p />").text(data[i]["description"]);
+                    var $cardMap = $("<p />").addClass("");
+
+    $cardContent.append($cardTitle);
+    $cardContent.append($cardMap);
+    $cardContent.append($cardDescription);
+    $cardContent.append($cardMap);
+    $cardContent.append($cardContent);
+
+    $cardImage.append($cardIcon);
+    $cardImage.append($btn);
+    $cardImage.append($imgCard);
+    $cardImage.append($cardImage);
+
+    
+    $card.append($cardImage);
+    $card.append($cardContent);
+    
+    $col.append($card);
+    $row.append($col);
+    
+  $("#after").append($row);
+    }
+}
+
+/*filtrado*/
+
+function filterPlaces() {
+    var search = $('#input').val().trim().toLowerCase();
+        var cardTitle = $(".card-title").text();
+        $(".card-title").each(function() {
+        if ($(this).text().toLowerCase().indexOf(search) === -1) {
+            $(this).parents(".card").hide()
+        } else {
+            $(this).parents(".card").show()
+        }
+    })
+}
+
+/*modal*/
+
+function modalPlaces() {
+    var name = $(this).find("nameM").text()
+    var parent = $(this).parent();
+    var grandParent = parent.parent();
+
+    $("#nameM").text(grandParent.find(".card-content").find(".card-title").text());
+
+    console.log();
+
+}
+
--- js/data.js
+++ js/data.js
@@ -4,13 +4,17 @@ var data = [
 {
         'name': 'Gatolicos',
         'adress': 'Tabasco 337, Cuauhtémoc, 06700 Ciudad de México, CDMX',
-        'food': 'Carne',
+        'description': 'I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.',
         'photo': 'http://www.lagateria.com/wp-content/uploads/2015/05/Cafe3-1024x768.jpg',
 },
     {
-      'name': 'VeganCat',
+        'name': 'Vegancats',
         'adress': 'Tabasco 337, Cuauhtémoc, 06700 Ciudad de México, CDMX',
-        'food': 'Vegan',
-        'photo': 'http://www.lagateria.com/wp-content/uploads/2015/05/Cafe3-1024x768.jpg',
-    },
+        'description': 'I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.',
+        'photo': 'assets/images/cat-1.jpg',
+        'url': "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3762.883660039213!2d-99.17048668701177!3d19.417432046201462!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d1ffce6d31ed79%3A0x234c9f56ff5077ee!2sLa+Gater%C3%ADa!5e0!3m2!1ses!2smx!4v1517602501494",
+
+},
+
+
 ]
