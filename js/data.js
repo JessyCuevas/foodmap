7d3d14db8e4425dcb209ce0b50cc48f89f7f16c0
JessyCuevas <jessy.cuevas.aranda@gmail.com>
2018-02-05 19:34:53 -0600

    actualizacion-Data

--- js/data.js
+++ js/data.js
@@ -16,5 +16,23 @@ var data = [
 
 },
 
+{
+        'name': 'Garros Galería',
+        'adress': 'Chihuahua 131, Roma Nte., 06700 Ciudad de México, CDMX',
+        'description': 'I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.',
+        'photo': 'https://photo620x400.mnstatic.com/d85ca289c2460bae1fd6b226618c3e16/garros-galeria-.jpg',
+        'url': "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3762.883660039213!2d-99.17048668701177!3d19.417432046201462!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d1ffce6d31ed79%3A0x234c9f56ff5077ee!2sLa+Gater%C3%ADa!5e0!3m2!1ses!2smx!4v1517602501494",
+
+},
+
+{
+        'name': 'Pet Central',
+        'adress': 'Av México 49, Condesa, 06100 Ciudad de México, CDMX',
+        'description': 'I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.',
+        'photo': 'http://dogzonenyc.com/wp-content/uploads/Pet-Central-247.jpg',
+        'url': "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3762.883660039213!2d-99.17048668701177!3d19.417432046201462!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d1ffce6d31ed79%3A0x234c9f56ff5077ee!2sLa+Gater%C3%ADa!5e0!3m2!1ses!2smx!4v1517602501494",
+
+},
+
 
 ]
